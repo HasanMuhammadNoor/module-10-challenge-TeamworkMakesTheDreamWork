52c3b735bf9d5d060b334045d99486e7dc73adc6
Hasan Noor <studiouskitten@imac.mynetworksettings.com>
2022-11-03 12:29:22 -0400

    I modified and wrote code in the engineer.js file.

--- library/engineer.js
+++ library/engineer.js
@@ -1,1 +1,16 @@
-class Engineer
+const Employee = require("./employee");
+
+class Engineer extends Engineer{
+    constructor(name,id,email,github){
+        super(name,id,github);
+        this.github = github;
+    }
+    getGitHub(){
+        return this.github;
+    }
+    getRole(){
+        return "Engineer";
+    }
+}
+
+module.exports = Engineer;
