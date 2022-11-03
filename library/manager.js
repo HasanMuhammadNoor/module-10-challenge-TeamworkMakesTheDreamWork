414516debec99b40f9f78dd7d2bda3d0def1f5ea
Hasan Noor <studiouskitten@imac.mynetworksettings.com>
2022-11-03 13:52:56 -0400

    I modified and wrote code in the manager.js file.

--- library/manager.js
+++ library/manager.js
@@ -1,1 +1,16 @@
-class Manager
+const Employee = require("./employee");
+
+class Manager extends Manager{
+    constructor(name,id,email,github){
+        super(name,id,github);
+        this.github = github;
+    }
+    getGitHub(){
+        return this.github;
+    }
+    getRole(){
+        return "Manager";
+    }
+}
+
+module.exports = Manager;
